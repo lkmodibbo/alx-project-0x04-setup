7cf67028aea7b67bd984ecb31bd79d23e0cb6a8d
lkmodibbo <lukmansaidmodibbo@gmail.com>
2025-11-27 18:54:57 +0100

    fix the layout probem

--- alx-project-0x05/pages/_app.tsx
+++ alx-project-0x05/pages/_app.tsx
@@ -1,7 +1,8 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { CountProvider } from "@/context/CountContext";
-import Layout from "@/components/layouts/layout";
+import Layout from "@/components/layouts/layout"; // lowercase `layout`
+
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
